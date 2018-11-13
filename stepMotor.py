88b830dbef11adeeb2219de2066353b484d76f78
Brady Paterson <brady.paterson@ibm.com>
2018-11-13 12:00:07 -0500

    Add step logic

--- stepMotor.py
+++ stepMotor.py
@@ -22,8 +22,24 @@ class stepMotor(object):
              GPIO.output(pin, 0)
 
     def rotate(self, n):
-        int(n)
+        #int(n)
         ## need to write this still
+        n = int(n)
+        if n < 0:
+            ii = -1
+        elif n > 0:
+            ii = 1
+        else:
+            return
+        if ii != self.prev:
+            print('re-aligning')
+            n += 24*ii
+        for i in range(int(abs(n))):
+          for halfstep in range(8):
+            for pin in range(4):
+              GPIO.output(self.control_pins[pin], self.halfstep_seq[::ii][halfstep][pin])
+            time.sleep(0.002)
+        self.prev = ii
 
 
     def close(self):

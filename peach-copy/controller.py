9cf1669aa045abe62cce5b30be02a7266c4e47f5
Peter May <Peter.May@ibm.com>
2019-02-13 21:21:02 -0500

    unclutter

--- peach-copy/controller.py
+++
@@ -1,7 +0,0 @@
-from stepMotor import stepMotor
-from consts import leftMotor, rightMotor
-
-stepMotor(leftMotor)
-stepMotor(rightMotor)
-
-
--- peach-copy/square.py
+++
@@ -1,6 +0,0 @@
-from stepMotor import stepMotor
-from consts import leftMotor, rightMotor
-
-left = stepMotor(leftMotor)
-right stepMotor(rightMotor)
-

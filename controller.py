0f6485a9030ac3e2b84cf77089bcdda537a5f247
Brady Paterson <brady.paterson@ibm.com>
2019-02-18 10:44:28 -0500

    diag

--- controller.py
+++ controller.py
@@ -6,7 +6,7 @@ vertical_pins = [31,33,35,37]
 
 horizontalMotor = stepMotor(horizontal_pins)
 verticalMotor = stepMotor(vertical_pins)
-## Create a diagonal line
+
 horizontalMotor.rotate(40)  # move cursor 40 units right
 horizontalMotor.rotate(-40) # move cursor 40 units left
 verticalMotor.rotate(40)  # move cursor 40 units up
@@ -14,6 +14,12 @@ verticalMotor.rotate(-40) # move cursor 40 units down
 
 horizontalMotor.close()
 verticalMotor.close()
+
+## Create a diagonal line
+# alternate instructions to the horizontal and vertical motors
+for step in range(40):
+	horizontalMotor.rotate(1)
+	verticalMotor.rotate(1)
 
 
 ## calibration

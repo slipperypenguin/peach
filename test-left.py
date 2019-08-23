926fa1b714753d1b70dc5f125cd0ef49ec765994
Peter May <pmay804@gmail.com>
2019-08-23 00:52:30 -0400

    backup

---
+++ test-left.py
@@ -0,0 +1,36 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+DIR = 11   # Direction GPIO Pin
+STEP = 15  # Step GPIO Pin
+CW = 1     # Clockwise Rotation
+CCW = 0    # Counterclockwise Rotation
+SPR = 148   # Steps per Revolution (360 / 7.5)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+GPIO.output(DIR, CW)
+
+#GPIO.output(STEP, GPIO.LOW)
+#GPIO.output(DIR, GPIO.LOW)
+
+
+step_count = SPR
+delay = .00208
+
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+sleep(.5)
+GPIO.output(DIR, CCW)
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+GPIO.cleanup()
